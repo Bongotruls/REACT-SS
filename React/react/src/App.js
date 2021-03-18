a64503fb64516d5cdf4bd6d545d742046f47e9ad
Bongotruls <78037422+Bongotruls@users.noreply.github.com>
2021-03-18 23:01:57 +0100

    lagt til sanity og react

---
+++ React/react/src/App.js
@@ -0,0 +1,12 @@
+import Movie from './components/Movies';
+
+function App() {
+  return (
+    <div>
+      <h1>App is working!</h1>
+      <Movie />
+    </div>
+  );
+}
+
+export default App;
---
+++ React/react/src/utils/movieService.js
@@ -0,0 +1,12 @@
+import client from './client';
+
+const movieListFields = `
+name, 
+"actor": actor->actor,
+"imageUrl": bilde.asset->url
+`;
+
+export const getMovies = async() => {
+    const data = await client.fetch(`*[_type == "movie"]{${movieListFields}}`);
+    return data;
+}
